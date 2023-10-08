8ea2ad37a73bcd7d6af5635ce9ed4165e6b5f198
Junology <jtachan@hotmail.com>
2023-10-08 20:53:30 +0900

    chore: add imports in the root file

--- Fifteen.lean
+++ Fifteen.lean
@@ -1,1 +1,15 @@
+import Fifteen.Logic.Basic
+import Fifteen.Logic.BishopFin
+import Fifteen.Data.Nat.Basic
+import Fifteen.Data.Fin.Basic
+import Fifteen.Data.List.Basic
+import Fifteen.Data.Array.Basic
+import Fifteen.Data.Permutation.Basic
+import Fifteen.Data.Permutation.Inversion
+import Fifteen.Data.Permutation.Sign
+import Fifteen.Data.Permutation.Random
+import Fifteen.Data.Board.Basic
+import Fifteen.Data.Board.Html
+
 def hello := "world"
+
