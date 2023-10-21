79a365e3faf3db8bae978dd30851b8f6cc40e736
Junology <jtachan@hotmail.com>
2023-10-21 17:21:10 +0900

    chore: remove unnecessary import

--- Fifteen/Data/Board/Html.lean
+++ Fifteen/Data/Board/Html.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 -/
 
 import ProofWidgets.Component.HtmlDisplay
-import ProofWidgets.Demos.InteractiveSvg
 
 import Fifteen.Data.Board.Basic
 
